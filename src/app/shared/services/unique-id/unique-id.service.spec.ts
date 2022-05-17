cafc55563a65513942d3e41cd61dc2d564bec115
Adenilson-Developers <adenilson.developers@gmail.com>
2022-05-17 17:00:29 -0300

    testando duplicidades de ids

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -7,6 +7,15 @@ describe(UniqueIdService.name, () => {
         const service = new UniqueIdService();
 
         const id = service.generatedUniqueIdWithPrefix('app');
-        expect(id).toContain('app-');
+        expect(id.startsWith('app-')).toBeTrue();
     });
+
+    // testando duplicidade dos ids gerados
+    it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times `,()=>{
+
+        const service = new UniqueIdService();
+        const firstId = service.generatedUniqueIdWithPrefix('app');
+        const secoundId = service.generatedUniqueIdWithPrefix('app');
+        expect(firstId).not.toBe(secoundId);
+    })
 });
