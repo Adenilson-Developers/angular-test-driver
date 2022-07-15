db80a1dfe3d81654597fececb94cb710a8305b74
Adenilson-Developers <adenilson.developers@gmail.com>
2022-07-14 22:29:44 -0300

    adicionando onGange no photo

--- src/app/shared/components/photo-board/photo-board.component.ts
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
 import { Photo } from "./interfaces/photo";
 
 
@@ -9,10 +9,30 @@ import { Photo } from "./interfaces/photo";
     
 })
 
-export class PhotoBoardComponent {
-    
-    @Input() public photos: Photo[] = [];
+export class PhotoBoardComponent implements OnChanges {
 
+    @Input()
+    public photos!: Photo[];
     public rows: any[][] = [];
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        for(let photos in changes){
+            let change = changes[photos];
+            let curVal = change.currentValue;
+            this.rows = this.groupColumns(curVal);
+        }
+    }
+
+    public groupColumns(photos: Photo[]): any[][] {
+        const newRows = [];
+        const step = 0;
+
+        for(let index = 0; index < photos.length; index += step){
+            newRows.push(photos.splice(index, index + step));
+        }
+        return newRows;
+    }
+
 }
+
 
