33beadbf1fe88dc94442c0512c6a6d55aac8116f
Adenilson-Developers <adenilson.developers@gmail.com>
2022-07-20 16:23:25 -0300

    injeção de serviço no teste

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,3 +1,4 @@
+import { PhotoBoardService } from 'src/app/shared/components/photo-board/service/photo-board.service';
 import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 import { PhotoListComponent } from "./photo-list.component";
@@ -7,6 +8,7 @@ describe(PhotoListComponent.name, ()=>{
     
     let fixture: ComponentFixture<PhotoListComponent>;
     let component: PhotoListComponent;
+    let service: PhotoBoardService
 
     beforeEach( async ()=> {
 
@@ -19,11 +21,14 @@ describe(PhotoListComponent.name, ()=>{
 
         fixture = TestBed.createComponent(PhotoListComponent);
         component = fixture.componentInstance;
+        service = TestBed.inject(PhotoBoardService);
     });
 
     it('Should create component',()=>{
         expect(component).toBeTruthy();
-    })
+    });
 
-
+    it('(D)hould  display board when data arrives',()=>{
+        fixture.detectChanges();
+    });
 })
