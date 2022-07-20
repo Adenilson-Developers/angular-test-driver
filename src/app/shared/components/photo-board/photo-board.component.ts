bb6bd8349bcff6bc9d7cedc59356b38a1e8c4ce5
Adenilson-Developers <adenilson.developers@gmail.com>
2022-07-19 23:16:38 -0300

    adicionando teste no photo-board component

--- src/app/shared/components/photo-board/photo-board.component.ts
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -26,7 +26,7 @@ export class PhotoBoardComponent implements OnChanges {
         // }
     }
 
-    public groupColumns(photos: Photo[]): any[][] {
+    private groupColumns(photos: Photo[]): any[][] {
         const newRows = [];
         const step = 4;
 
