656319879083c73b66a5dfdf8018c9567fa1a7c5
Adenilson-Developers <adenilson.developers@gmail.com>
2022-07-21 20:24:36 -0300

    adicionando afterEach

--- src/app/shared/components/photo-board/service/photo-board.service.spec.ts
+++ src/app/shared/components/photo-board/service/photo-board.service.spec.ts
@@ -30,6 +30,8 @@ describe(PhotoBoardService.name,()=> {
         httpController = TestBed.inject(HttpTestingController);
     });
 
+    afterEach(()=>{ httpController.verify()});
+    
     it(`#${PhotoBoardService.prototype.getPhotos.name} Should return photos with description in  uppercase`, done => {
        
         service.getPhotos().subscribe(photos => {
